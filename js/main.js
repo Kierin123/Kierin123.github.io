77efaf266ccf9fee22f9160d47dff08f681e1e7f
Kierinkiewicz Marcin <marcin.kierinkiewicz@etteplan.com>
2025-11-29 23:11:14 +0100

    add files

---
+++ js/main.js
@@ -0,0 +1,12 @@
+document.getElementById('year').textContent=new Date().getFullYear();
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+      observer.unobserve(entry.target);
+    }
+  });
+}, { threshold: 0.2 });
+
+document.querySelectorAll(".fade-in").forEach(el => observer.observe(el));
