77cbd4a155c76cb2de6e176b683a5a597fe9c3d0
Kierinkiewicz Marcin <marcin.kierinkiewicz@etteplan.com>
2025-12-08 21:51:06 +0100

    fix

--- js/main.js
+++ js/main.js
@@ -10,3 +10,11 @@ const observer = new IntersectionObserver(entries => {
 }, { threshold: 0.2 });
 
 document.querySelectorAll(".fade-in").forEach(el => observer.observe(el));
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("active");
+});
